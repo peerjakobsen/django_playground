5aa34279aa1a4501f3a6073ae190d02b0b881680
Jakobsen <peerjako@88665a3b0626.ant.amazon.com>
2024-01-23 07:47:25 +0100

    Add all auth and change default allauth templates to use site base.html including pico.

--- dp_project/settings.py
+++ dp_project/settings.py
@@ -34,6 +34,12 @@ INSTALLED_APPS = [
     # My apps.
     'apps.core',
 
+    # AllAuth apps.
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    # ... include the providers you want to enable:
+    'allauth.socialaccount.providers.github',    
     # Default django apps.
     'django.contrib.admin',
     'django.contrib.auth',
@@ -51,9 +57,17 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    # Add the account middleware:
+    "allauth.account.middleware.AccountMiddleware",
 ]
 
+# Provider specific settings
+SOCIALACCOUNT_PROVIDERS = {
+}
+
 ROOT_URLCONF = 'dp_project.urls'
+
+LOGIN_REDIRECT_URL = '/'
 
 TEMPLATES = [
     {
@@ -66,9 +80,19 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                # `allauth` needs this from django
+                'django.template.context_processors.request',
             ],
         },
     },
+]
+
+AUTHENTICATION_BACKENDS = [
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+
+    # `allauth` specific authentication methods, such as login by email
+    'allauth.account.auth_backends.AuthenticationBackend',
 ]
 
 WSGI_APPLICATION = 'dp_project.wsgi.application'
